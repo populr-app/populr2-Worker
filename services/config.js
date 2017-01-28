e6c2dcf7af3a47b3a103bed37154b80224fd9132
Garrett Cox <garrettjcox@gmail.com>
2017-01-28 16:54:49 -0600

    Playing around with twitter API

--- services/config.js
+++ services/config.js
@@ -4,5 +4,11 @@ module.exports = require('rc')('populr', {
     log: {
       level: 'error',
     },
+    twitter: {
+      consumer_key: 'Configure in .populrrc',
+      consumer_secret: 'Configure in .populrrc',
+      access_token_key: 'Configure in .populrrc',
+      access_token_secret: 'Configure in .populrrc',
+    },
   },
 }).worker;
--- tasks/twitter.js
+++ tasks/twitter.js
@@ -1,2 +1,41 @@
 
-module.exports = () => Promise.reject('Yet to be implemented');
+const TwitterApi = require('twitter');
+const _ = require('lodash');
+
+const client = new TwitterApi(config.twitter);
+const people = [
+  { id: '822215679726100480' },
+  { id: '17919972' },
+  { id: '27260086' },
+];
+
+module.exports = () => {
+  return getUsers()
+    .then(splitIntoChunks)
+    .then(getTwitterData)
+    .then(updatePeople);
+};
+
+function getUsers() {
+  return Promise.resolve(people);
+}
+
+function splitIntoChunks(users) {
+  return _.chunk(users, 1);
+}
+
+function getTwitterData(chunks) {
+  const promiseArray = [];
+
+  chunks.forEach((chunk) => {
+    const ids = _.map(chunk, 'id').join();
+    promiseArray.push(client.get('users/lookup', { user_id: ids }));
+  });
+
+  return Promise.all(promiseArray)
+    .then(results => _.flatten(results));
+}
+
+function updatePeople(results) {
+  console.log(results);
+}
